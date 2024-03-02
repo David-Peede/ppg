4c481ad5c3ded9b01c48b0a6ddebd3c3e7719dad
David Peede <david_peede@brown.edu>
2024-03-02 09:24:38 -0500

    identify_duplicate_records.py is up and ready

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ppg
+
 Plasmodium Pop-Gen
+
+## `vcfs`
+
+`identify_duplicate_records.py`
+
+```bash
+# Example usage.
+for CHR in 01 02 03 04 05 06 07 08 09 10 11 12 13 14; do
+sbatch -J Pf3D7_${CHR}_v3.pf.identify_dups -N 1 -n 1 -t 1-0 --mem=2G -p batch --account=ccmb-condo -o logs/Pf3D7_${CHR}_v3.pf.identify_dups-%A.out -e logs/Pf3D7_${CHR}_v3.pf.identify_dups-%A.err --mail-type=FAIL --mail-user=david_peede@brown.edu --wrap="module load tabix python; python ./ppg/vcfs/identify_duplicate_records.py /users/dpeede/data/dpeede/13_plasmodium/vcf_data/init_vcfs/Pf3D7_${CHR}_v3.pf7.vcf.gz Pf3D7_${CHR}_v3 pf7 /users/dpeede/data/dpeede/13_plasmodium/vcf_data/dup_info | bgzip > /users/dpeede/data/dpeede/13_plasmodium/vcf_data/dup_info/Pf3D7_${CHR}_v3.pf7.dup_records.vcf.gz"
+done
+```
---
+++ vcfs/identify_duplicate_records.py
@@ -0,0 +1,78 @@
+### Dependencies ###
+import gzip
+import sys
+
+## sys.argv[1] = vcf ##
+## sys.argv[2] = chromosome ##
+## sys.argv[3] = dataset suffix ##
+## sys.argv[4] = duplicate record path ##
+
+
+# Define a function to identify what duplicated entires from a vcf.
+def identify_dups(vcf, chrom, suffix, dup_path):
+    # Intialize a list to store the regions file information.
+    dup_file_lines = []
+    # Intialize a list to store the duplicated records vcf information.
+    dup_vcf_lines = []
+    # Intilailize the previous line and position.
+    prev_line = None
+    prev_pos = None
+    # Inialize a set to store duplicated positions.
+    dup_set = set()
+    # Read the vcf file and intilialize the duplicated sites regions file.
+    with gzip.open(vcf, 'rt') as data, \
+         gzip.open(f'{dup_path}/{chrom}.{suffix}.dup_sites.txt.gz', 'wt') as dup_file:
+        # Iterate through every line in the original vcf file.
+        for line in data:
+            # If the line contains meta or header information.
+            if line.startswith('#'):
+                # Write the line to the duplicated sites vcf list.
+                dup_vcf_lines.append(line)
+             # Else, the line contains genotype information.
+            else:
+                # Grab the current poistion.
+                pos = line.split()[1]
+                # If the current position is already known to be duplicated.
+                if pos in dup_set:
+                    # Append the current line to the duplicated sites vcf file list.
+                    dup_vcf_lines.append(line)
+                # Else-if the current position is the same as the previous position.
+                elif pos == prev_pos:
+                    # Add the position to the duplicated set.
+                    dup_set.add(pos)
+                    # Append the duplicated position to the regions file list.
+                    dup_file_lines.append(chrom + '\t' + pos + '\n')
+                    # Append the previous line to the duplicated sites vcf file list.
+                    dup_vcf_lines.append(prev_line)
+                    # Append the current line to the duplicated sites vcf file list.
+                    dup_vcf_lines.append(line)
+                # Else, the current position is unique.
+                else:
+                    # Update the previous line and position.
+                    prev_line = line
+                    prev_pos = pos
+            # If there are 2_500 lines in the duplicated records regions file list.
+            if len(dup_file_lines) == 2_500:
+                # Write the duplicated sites information to the duplicated regions file
+                # in chunks to improve performance.
+                dup_file.writelines(dup_file_lines)
+                # Clear the written lines.
+                dup_file_lines.clear()
+            # If there are 2_500 lines in the duplicated records vcf file list.
+            if len(dup_vcf_lines) == 2_500:
+                # Write the duplicted vcf lines to the vcf file in chunks to improve performance.
+                sys.stdout.writelines(dup_vcf_lines)
+                # Clear the written lines.
+                dup_vcf_lines.clear()
+        # If there are still duplicated records regions file lines to be written.
+        if dup_file_lines:
+            # Write the remaining lines.
+            dup_file.writelines(dup_file_lines)
+        # If there are still duplicated records vcf lines to be written.
+        if dup_vcf_lines:
+            # Write the remaining lines.
+            sys.stdout.writelines(dup_vcf_lines)
+
+
+# Generate the duplicated sites vcf and regions file.
+identify_dups(vcf=sys.argv[1], chrom=sys.argv[2], suffix=sys.argv[3], dup_path=sys.argv[4])
